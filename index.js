84081f731b71c8494e55ff9ee2f9af21999fdecb
William Holmes <holmes.william@gmail.com>
2018-03-13 13:33:01 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const webshot = require('webshot');
+
+const logger = require('winston');
+
+const del = require('delete');
+const moment = require('moment');
+const npmAPI = require('api-npm');
+const Q  = require('q');
+const _ = require('underscore');
+
+const appFramework = require('watsonworkspace-bot');
+appFramework.level('verbose');
+appFramework.startServer();
+const app = appFramework.create();
+
+const UI = require('watsonworkspace-sdk').UI;
+
+const constants = require('./js/constants');
+const packages = require('./js/packages');
+const strings = require('./js/strings');
+
+app.authenticate().then(() => app.uploadPhoto('./appicon.jpg'));
+
+
+app.on('message-created', (message, annotation) => {
+    const { content = '', spaceId } = message;
+    _.each(content.match(constants.regex.NPM), p => {
+        const packageName = strings.chompLeft(p, constants.regex.KEY);
+        packages.getAnnotation(packageName).then(data => {
+            app.sendMessage(spaceId, data);
+            // const URL = strings.substitue(constants.urls.NPM, [packageName]);
+            // const filePath = `./webshots/npm_${packageName}.png`;
+            // webshot(URL, filePath, err => {
+            //     app.sendMessage(spaceId, data);
+            //     if (_.isEmpty(err)) {
+            //         app.sendFile(spaceId,filePath);
+            //         del.sync(filePath, { force: true });
+            //     }
+            // });
+        });
+    });
+});
+
+app.on('actionSelected:/NPM', (message, annotation, params) => {
+    const { userId } = message;
+    const promises = _.map(params, packages.getCard);
+    if(_.isEmpty(promises)) {
+        app.sendTargetedMessage(userId, annotation, UI.generic(constants.NOT_FOUND, ''))
+    } else {
+        Q.allSettled(promises).then(data => {
+            const cards = _.pluck(data, 'value');
+            app.sendTargetedMessage(userId, annotation, cards);
+        });
+    }
+});
+
+app.on('actionSelected', (message, annotation) => {
+   const { actionId } = annotation;
+   if (actionId.includes(constants.ACTION_ID)) {
+       const packageName = strings.chompLeft(actionId, constants.ACTION_ID);
+       packages.getAnnotation(packageName).then(data => {
+            const { spaceId, userId } = message;
+            app.sendMessage(spaceId, data);
+            app.sendTargetedMessage(userId, annotation, UI.generic(packageName, constants.PACKAGE_SHARED))
+       });
+   }
+});
